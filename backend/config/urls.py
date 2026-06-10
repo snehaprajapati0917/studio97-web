25294c5cb53e7f67e2ff0507d0a1dd79650c5572
sneha <snehaprajapati0917@gmai.com>
2026-06-10 19:01:08 +0530

    Add home route

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,7 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+def home(request):
+    return HttpResponse("Studio97 Backend is Running Successfully 🚀")
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/booking/', include('booking.urls')),
     path(
