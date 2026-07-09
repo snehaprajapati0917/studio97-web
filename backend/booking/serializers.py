ae62e7f5642cc3cc7d1d9628609fa290287406f1
sneha <snehaprajapati0917@gmai.com>
2026-07-09 19:09:31 +0530

    contact validation

--- backend/booking/serializers.py
+++ backend/booking/serializers.py
@@ -1,9 +1,23 @@
 from rest_framework import serializers
 from .models import Booking
 
+# class BookingSerializer(serializers.ModelSerializer):
+
+#     class Meta:
+#         model = Booking
+#         fields = "__all__"
+#         from rest_framework import serializers
+# from .models import Booking
+
 class BookingSerializer(serializers.ModelSerializer):
+
+    def validate_phone(self, value):
+        if len(value) != 10 or not value.isdigit():
+            raise serializers.ValidationError(
+                "Phone number must be exactly 10 digits."
+            )
+        return value
 
     class Meta:
         model = Booking
         fields = "__all__"
-        
