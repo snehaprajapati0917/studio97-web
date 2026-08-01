740d7d7890e8c03f03b8609cfdb60d5e58a7ad01
sneha <snehaprajapati0917@gmai.com>
2026-08-01 21:56:49 +0530

    database commit

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -94,18 +94,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.getenv("DATABASE_URL")
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=True,
     )
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 
 # # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
